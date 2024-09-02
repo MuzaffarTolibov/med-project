7f6bf2ad55f21dbf62bfaeb2113abe0694e64f30
Команда Разработки <afkor.devteam@gmail.com>
2024-09-02 15:46:49 +0500

    muzaffar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: 'home',
-    loadComponent: () => import('./features/main/home/home.component').then(m => m.HomeComponent),
+    path: 'main',
+    loadChildren: () => import('./features/main/main.routes').then(m => m.MAIN_ROUTES)
   }
 ];
--- src/app/features/main/main.routes.ts
+++ src/app/features/main/main.routes.ts
@@ -1,3 +1,24 @@
 import { Routes } from '@angular/router';
 
-export const MAIN_ROUTES: Routes = []
+export const MAIN_ROUTES: Routes = [
+  {
+    path: '',
+    redirectTo: 'home',
+    pathMatch: 'full',
+  },
+  {
+    path: '',
+    loadComponent: () => import('./main.component').then(m => m.MainComponent),
+    children: [
+      {
+        path: 'home',
+        loadComponent: () => import('./home/home.component').then(m => m.HomeComponent),
+      }
+    ]
+  },
+  {
+    path: '**',
+    redirectTo: '404'
+  }
+]
+
