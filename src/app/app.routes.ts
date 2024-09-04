5dc5d976de5904301313e1ac719376470b535efd
Команда Разработки <afkor.devteam@gmail.com>
2024-09-04 15:42:50 +0500

    muzaffar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,11 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: '',
+    redirectTo: 'main',
+    pathMatch: 'full',
+  },
+  {
     path: 'main',
     loadChildren: () => import('./features/main/main.routes').then(m => m.MAIN_ROUTES)
   }
--- src/app/shared/components/button/button.component.ts
+++ src/app/shared/components/button/button.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,11 +8,21 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './button.component.html',
   styleUrl: './button.component.scss'
 })
-export class ButtonComponent {
+export class ButtonComponent implements OnInit{
   @Input() title: string | undefined;
   @Input() customStyle: {[key:string]: any} | undefined;
   @Input() btnClass: string | undefined;
   @Input() customClass: string = '';
+  @Input() icon: string | undefined;
   @Output() clicked = new EventEmitter<MouseEvent>();
   @Output() dbclick = new EventEmitter<string>();
+
+  imgPath: string = '';
+
+  constructor() {
+  }
+
+  ngOnInit() {
+    this.imgPath = `assets/btn-icon/${this.icon}.svg`;
+  }
 }
