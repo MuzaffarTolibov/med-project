172b5dd5a437c97b2cc35d3db3414d3d965b910c
Команда Разработки <afkor.devteam@gmail.com>
2024-09-23 15:37:51 +0500

    muzaffar

--- src/app/features/main/home/home.component.ts
+++ src/app/features/main/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { FormComponent } from "../../../shared/components/form/form.component";
 import { InfoCardComponent } from "../../../shared/components/info-card/info-card.component";
+import { ButtonComponent } from "../../../shared/components/button/button.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormComponent, InfoCardComponent],
+  imports: [FormComponent, InfoCardComponent, ButtonComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
