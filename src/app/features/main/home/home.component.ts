183b8cdd41f2b23291d98239785c0b4fb134564f
Команда Разработки <afkor.devteam@gmail.com>
2024-09-30 15:37:32 +0500

    muzaffar

--- src/app/features/main/home/home.component.ts
+++ src/app/features/main/home/home.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { FormComponent } from "../../../shared/components/form/form.component";
 import { InfoCardComponent } from "../../../shared/components/info-card/info-card.component";
 import { ButtonComponent } from "../../../shared/components/button/button.component";
+import { FooterComponent } from "../../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormComponent, InfoCardComponent, ButtonComponent],
+  imports: [FormComponent, InfoCardComponent, ButtonComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
--- src/app/features/main/main.component.ts
+++ src/app/features/main/main.component.ts
@@ -1,14 +1,16 @@
 import { Component } from '@angular/core';
 import { MenuComponent } from '../../shared/layout/menu/menu.component';
 import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from "../../shared/components/footer/footer.component";
 
 @Component({
   selector: 'app-main',
   standalone: true,
   imports: [
     MenuComponent,
-    RouterOutlet
-  ],
+    RouterOutlet,
+    FooterComponent
+],
   templateUrl: './main.component.html',
   styleUrl: './main.component.scss'
 })
