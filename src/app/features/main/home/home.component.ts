f644dad9971cec8b3d6bab21c01ec52ca24b81f6
Команда Разработки <afkor.devteam@gmail.com>
2024-10-23 15:55:04 +0500

    muzaffar

--- src/app/features/main/home/home.component.ts
+++ src/app/features/main/home/home.component.ts
@@ -3,11 +3,12 @@ import { FormComponent } from "../../../shared/components/form/form.component";
 import { InfoCardComponent } from "../../../shared/components/info-card/info-card.component";
 import { ButtonComponent } from "../../../shared/components/button/button.component";
 import { FooterComponent } from "../../../shared/components/footer/footer.component";
+import { ModalComponent } from "../../../shared/components/modal/modal.component";
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [FormComponent, InfoCardComponent, ButtonComponent, FooterComponent],
+  imports: [FormComponent, InfoCardComponent, ButtonComponent, FooterComponent, ModalComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
